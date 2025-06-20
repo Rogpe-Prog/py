34bb82046376cd19e93df69e8b5a7de6a2d14dc3
Rogpe-Prog <paulo.rogpe@gmail.com>
2025-06-20 17:04:57 -0300

    strings

--- Strings/String formatting/string_formatting.py
+++ Strings/String formatting/string_formatting.py
@@ -1,5 +1,6 @@
 name = "John"
+age = 22
 # Note: %s is inside the string, % is after the string.
 print("Hello, PyCharm! My name is %s!" % name)
 
-print("I'm ??? years old" '???')
+print("I'm %d years old" % age)
